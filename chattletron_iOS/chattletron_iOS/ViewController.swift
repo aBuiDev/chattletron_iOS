534b410e8357022f5a4381133ec6d742db7196b5
Andrew Bui <andrew.bui.au@gmail.com>
2021-10-19 14:53:12 +1100

    initialised project and added MessageKit dependency

---
+++ chattletron_iOS/chattletron_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chattletron_iOS
+//
+//  Created by Andrew Bui on 19/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
